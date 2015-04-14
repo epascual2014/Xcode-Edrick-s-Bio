8d1aed74f1366e3f1a6ac79f317383c4200ce912
Edrick Pascual <edrickpascual@yahoo.com>
2015-04-13 20:14:56 -0700

    Initial Commit

---
+++ Edrick's Intro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Edrick's Intro
+//
+//  Created by Edrick on 4/13/15.
+//  Copyright (c) 2015 Edrick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
